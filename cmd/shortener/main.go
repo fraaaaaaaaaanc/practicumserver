9b892776cdcead16bd2a4c76c5c51e98a9772382
flyalwayswannatofly <frantsev_94@mail.ru>
2023-09-05 12:36:38 +0300

    fix GetRequest five

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-var shorturls map[string][]byte = make(map[string][]byte)
+var shorturls map[string]string = make(map[string]string)
 var LocalURL string = "http://localhost:8080/"
 
 const (
@@ -28,6 +28,53 @@ func run() error {
 	router.HandleFunc("/{id:[a-zA-Z0-9]+}", GetRequest).Methods("GET")
 
 	return http.ListenAndServe(`:8080`, router)
+}
+
+func GetRequest(w http.ResponseWriter, r *http.Request) {
+	if _, ok := shorturls[r.URL.String()[1:]]; !ok {
+		//r.Header.Get("Content-Type") != "text/plain" ||
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(ok)
+		return
+	}
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	//w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Location", shorturls[r.URL.String()[1:]])
+	_, _ = w.Write([]byte(shorturls[r.URL.String()[1:]]))
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain; charset=utf-8" || r.URL.String() != "/" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		//http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	//rand.Seed(time.Now().UnixNano())
+	id := Base62Encode(rand.Uint64())
+	if _, ok := shorturls[id]; !ok {
+		shorturls[id] = string(body)
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(LocalURL + id))
+}
+
+func Base62Encode(number uint64) string {
+	length := len(alphabet)
+	var encodedBuilder strings.Builder
+	encodedBuilder.Grow(10)
+	for ; number > 0; number = number / uint64(length) {
+		encodedBuilder.WriteByte(alphabet[(number % uint64(length))])
+	}
+
+	return encodedBuilder.String()
 }
 
 //func webhook(w http.ResponseWriter, r *http.Request) {
@@ -65,50 +112,3 @@ func run() error {
 //		return
 //	}
 //}
-
-func GetRequest(w http.ResponseWriter, r *http.Request) {
-	if _, ok := shorturls[r.URL.String()[1:]]; !ok {
-		//r.Header.Get("Content-Type") != "text/plain" ||
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Println(ok)
-		return
-	}
-	w.WriteHeader(http.StatusTemporaryRedirect)
-	//w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("Location", string(shorturls["OTv0FdGU8Ng"]))
-	//_, _ = w.Write([]byte(w.Header().Get("Location")))
-}
-
-func PostRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Content-Type") != "text/plain; charset=utf-8" || r.URL.String() != "/" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		//http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-	//rand.Seed(time.Now().UnixNano())
-	id := Base62Encode(rand.Uint64())
-	if _, ok := shorturls[id]; !ok {
-		shorturls["OTv0FdGU8Ng"] = body
-	}
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusCreated)
-	_, _ = w.Write([]byte(LocalURL + id))
-}
-
-func Base62Encode(number uint64) string {
-	length := len(alphabet)
-	var encodedBuilder strings.Builder
-	encodedBuilder.Grow(10)
-	for ; number > 0; number = number / uint64(length) {
-		encodedBuilder.WriteByte(alphabet[(number % uint64(length))])
-	}
-
-	return encodedBuilder.String()
-}
