1adc16692f3f327c15e60ddd516caa0a48d5e3e8
flyalwayswannatofly <frantsev_94@mail.ru>
2023-09-05 11:33:50 +0300

    fix GerRequest

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"math/rand"
 	"net/http"
@@ -21,53 +23,59 @@ func main() {
 }
 
 func run() error {
-	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
+	router := mux.NewRouter()
+	router.HandleFunc("/", PostRequest).Methods("POST")
+	router.HandleFunc("/{id:[a-zA-Z0-9]+}", GetRequest).Methods("GET")
+
+	return http.ListenAndServe(`:8080`, router)
 }
 
-func webhook(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		GetRequest(w, r)
-	case http.MethodPost:
-		if err := r.ParseForm(); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		PostRequest(w, r)
-	}
-	//	if r.Method == http.MethodPost && r.Header.Get("Content-Type") == "text/plain" {
-	//		//w.Header().Set("Content-Type", "application/json")
-	//		//// пока установим ответ-заглушку, без проверки ошибок
-	//		//_, _ = w.Write([]byte(`
-	//		// {
-	//		//   "response": {
-	//		//     "text": "Извините, я пока ничего не умею"
-	//		//   },
-	//		//   "version": "1.0"
-	//		// }`))
-	//		_, err := io.ReadAll(r.Body)
-	//		if err != nil {
-	//			http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
-	//			return
-	//		}
-	//		defer r.Body.Close()
-	//
-	//		w.Header().Set("Content-Type", "text/plain")
-	//		_, _ = w.Write([]byte(r.Header.Get("Content-Length")))
-	//	} else {
-	//		w.WriteHeader(http.StatusBadRequest)
-	//		return
-	//	}
-}
+//func webhook(w http.ResponseWriter, r *http.Request) {
+//	switch r.Method {
+//	case http.MethodGet:
+//		GetRequest(w, r)
+//	case http.MethodPost:
+//		if err := r.ParseForm(); err != nil {
+//			w.WriteHeader(http.StatusBadRequest)
+//			return
+//		}
+//		PostRequest(w, r)
+//	}
+//	if r.Method == http.MethodPost && r.Header.Get("Content-Type") == "text/plain" {
+//		//w.Header().Set("Content-Type", "application/json")
+//		//// пока установим ответ-заглушку, без проверки ошибок
+//		//_, _ = w.Write([]byte(`
+//		// {
+//		//   "response": {
+//		//     "text": "Извините, я пока ничего не умею"
+//		//   },
+//		//   "version": "1.0"
+//		// }`))
+//		_, err := io.ReadAll(r.Body)
+//		if err != nil {
+//			http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
+//			return
+//		}
+//		defer r.Body.Close()
+//
+//		w.Header().Set("Content-Type", "text/plain")
+//		_, _ = w.Write([]byte(r.Header.Get("Content-Length")))
+//	} else {
+//		w.WriteHeader(http.StatusBadRequest)
+//		return
+//	}
+//}
 
 func GetRequest(w http.ResponseWriter, r *http.Request) {
 	if _, ok := shorturls[r.URL.String()[1:]]; r.Header.Get("Content-Type") != "text/plain" || !ok {
 		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(ok)
 		return
 	}
 	w.WriteHeader(http.StatusTemporaryRedirect)
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Location", string(shorturls[r.URL.String()[1:]]))
+	//_, _ = w.Write([]byte(w.Header().Get("Location")))
 }
 
 func PostRequest(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module shortener
 
-go 1.19
+go 1.21.0
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+)
